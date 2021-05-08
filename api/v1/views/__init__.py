512874862230962e23e2d39bf59f45996fd9f324
DiegoCol93 <dfelo93@gmail.com>
2021-05-07 20:52:13 -0500

    |- ADDED -| Shebang again.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """ Flask blueprint module. """
 from flask import Blueprint
 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ Module for storing indeces for the route Blueprints. """
 from api.v1.views import app_views
+from flask import jsonify
 from models.amenity import Amenity
+from models.city import City
 from models.review import Review
 from models.place import Place
 from models.state import State
-from models.city import City
 from models.user import User
 from models import storage
-from flask import jsonify
 
 
 @app_views.route("/status", strict_slashes=False)
