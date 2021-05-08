a500df714f09d79f0fe9e90834bbb5748ed81ea4
el-dani-cortes <danicor83@gmail.com>
2021-05-08 22:29:12 +0000

    UPDATED | Tests update a review object with update method with another code to find the checkers errors

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -47,66 +47,58 @@ def delete_review_obj(review_id):
     abort(404)
 
 
-@app_views.route('/places/<place_id>/reviews', methods=['POST'],
+@app_views.route("/places/<place_id>/reviews", methods=["POST"],
                  strict_slashes=False)
-def route_place_post(place_id):
-    """State POST Route 32c11d3d-99a1-4406-ab41-7b6ccb7dd760 user
-     place 3ebfaf23-cede-4cf0-964d-8afc17b11d02
-    """
-    place = storage.get(Place, place_id)
-    if place is None:
-        abort(404)
-    obj = request.get_json()
-    if obj is None:
-        return make_response("Not a JSON", 400)
-    if 'user_id' not in obj:
-        abort(400, 'Missing user_id')
-    user = storage.get(User, obj['user_id'])
-    if user is None:
-        abort(404)
-    if 'text' not in obj:
-        abort(400, 'Missing text')
-    obj['place_id'] = place_id
-    review = Review(**obj)
-    storage.new(review)
-    storage.save()
-    return make_response(jsonify(review.to_dict()), 201)
-
-
-# @app_views.route("/places/<place_id>/reviews", methods=["POST"],
-#                  strict_slashes=False)
-# def create_review_obj(place_id):
-#     """ Creates a new review linked to a place. """
-#     if storage.get(Place, place_id) is None:
-#         abort(404)
-#     data = request.get_json()
-#     if data is None:
-#         return(make_response('Not a JSON', 400))
-#     if 'user_id' not in data:
-#         abort(400, 'Missing user_id')
-#     if storage.get(User, data['user_id']) is None:
-#         abort(404)
-#     if 'text' not in data:
-#         abort(400, 'Missing text')
-#     data['place_id'] = place_id
-#     review = Review(**data)
-#     review.save()
-#     return(make_response(jsonify(review.to_dict()), 201))
-
-
-@app_views.route("/reviews/<review_id>", methods=["PUT"],
-                 strict_slashes=False)
-def update_review_obj(review_id):
-    """ Updates a review by its id. """
-    review = storage.get(Review, review_id)
-    if review is None:
+def create_review_obj(place_id):
+    """ Creates a new review linked to a place. """
+    if storage.get(Place, place_id) is None:
         abort(404)
     data = request.get_json()
     if data is None:
         return(make_response('Not a JSON', 400))
-    ignored_keys = ["id", "user_id", "place_id", "created_at", "updated_at"]
-    for key, value in data.items():
-        if key not in ignored_keys:
-            setattr(review, key, value)
+    if 'user_id' not in data:
+        abort(400, 'Missing user_id')
+    if storage.get(User, data['user_id']) is None:
+        abort(404)
+    if 'text' not in data:
+        abort(400, 'Missing text')
+    data['place_id'] = place_id
+    review = Review(**data)
+    review.save()
+    return(make_response(jsonify(review.to_dict()), 201))
+
+
+@app_views.route('/reviews/<review_id>', methods=['PUT'],
+                 strict_slashes=False)
+def reviews_put(review_id=None):
+    """ States PUT route """
+    ignore_keys = ['id', 'created_at', 'updated_at', 'place_id', 'user_id']
+    obj = request.get_json()
+    if obj is None:
+        return make_response("Not a JSON", 400)
+    review = storage.get(Review, review_id)
+    if review is None:
+        abort(404)
+    for k, v in obj.items():
+        if k not in ignore_keys:
+            setattr(review, k, v)
     storage.save()
-    return(make_response(jsonify(review.to_dict()), 200))
+    return make_response(jsonify(review.to_dict()), 200)
+
+
+# @app_views.route("/reviews/<review_id>", methods=["PUT"],
+#                  strict_slashes=False)
+# def update_review_obj(review_id):
+#     """ Updates a review by its id. """
+#     review = storage.get(Review, review_id)
+#     if review is None:
+#         abort(404)
+#     data = request.get_json()
+#     if data is None:
+#         return(make_response('Not a JSON', 400))
+#     ignored_keys = ["id", "user_id", "place_id", "created_at", "updated_at"]
+#     for key, value in data.items():
+#         if key not in ignored_keys:
+#             setattr(review, key, value)
+#     storage.save()
+#     return(make_response(jsonify(review.to_dict()), 200))
