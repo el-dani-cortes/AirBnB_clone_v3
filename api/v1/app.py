37d0607227f6974073b63d61c533d53343ebd0cd
el-dani-cortes <danicor83@gmail.com>
2021-05-08 13:47:19 +0000

    UPDATED | Test another file to find the checker errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python3
-""" Flask module, returns status of the api. """
-from api.v1.views import app_views
-from flask import Flask
+"""Module app flask"""
 from models import storage
-from os import getenv as env
+from flask import Blueprint, Flask, jsonify, make_response
+from os import environ
+from api.v1.views import app_views
+
+env = environ.copy()
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def close_connection(exception):
-    """ Closes the session's connection. """
+def teardown(exception):
+    """close storage"""
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(e):
-    """ Error handler 404, for Not found response."""
-    return {"error": "Not found"}, 404
+def page_not_found(error):
+    """ Error not found 404"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-if __name__ == "__main__":
-    host = env("HBNB_API_HOST", "0.0.0.0")
-    port = int(env("HBNB_API_PORT", "5000"))
-    app.run(host=host, port=port, threaded=True)
+if __name__ == '__main__':
+    if "HBNB_API_HOST" in env:
+        host_api = env['HBNB_API_HOST']
+    else:
+        host_api = '0.0.0.0'
+    if 'HBNB_API_PORT' in env:
+        host_port = env['HBNB_API_PORT']
+    else:
+        host_port = 5000
+    app.run(host=host_api, port=int(host_port), threaded=True, debug=True)
