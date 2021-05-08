a9f804d05ac7a443e5984d225de8c5ee097e5436
el-dani-cortes <danicor83@gmail.com>
2021-05-08 13:51:34 +0000

    UPDATED | Added shebang

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """ Flask blueprint module and class objects routes. """
 from flask import Blueprint
 
@@ -7,3 +8,5 @@ from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
